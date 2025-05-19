7493927e673b1f84c09c8d516f00d6dd9eeb4ab3
lana <bigtroublejake@gmail.com>
2025-05-18 21:36:09 -0400

    added a glomp all following module

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 A collection of tools intended to help out users on the David Social platform.
+
 https://www.davidsocial.com/
 
 A proper readme file will come in time.
---
+++ glomp_all_following.py
@@ -0,0 +1,26 @@
+import requests
+import json
+import davidsocial_login as login
+
+session = login.session
+
+# gets your following list
+res = requests.get(f"https://www.davidsocial.com/api/get-bootlicking?id={login.user}", cookies= session.cookies)
+
+# turns json list into python list
+following = json.loads(res.content)
+# print(following)
+
+
+# this loop actually does all the glomping
+for i in following:
+    glomp = {
+        "message": "",
+        "sender": login.user,
+        "receiver": i
+    }
+
+    res = requests.post("https://www.davidsocial.com/api/glomp", json= glomp, cookies= session.cookies)
+    # print(res)
+
+print("finished glomping")
