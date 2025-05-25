b107e86d9e9c775735dce88e7055d7bceec3f5ee
lana <>
2025-05-25 19:39:22 -0400

    commented search_user.py

--- modules/search_user.py
+++ modules/search_user.py
@@ -10,9 +10,11 @@ session = login.session
 
 user = input("Enter user: ")
 
+# gets all of selected user's posts
 res = requests.get(f"https://www.davidsocial.com/api/user-posts?username={user}", cookies= session.cookies)
-
 ls = res.json()
+
+# checks if user exists
 if not ls:
     print("User doesn't exist, or hasn't made any posts yet")
     time.sleep(2)
@@ -20,8 +22,8 @@ if not ls:
 
 
 find = input("Search for: ").lower()
-# print(find)
 
+# finds text in posts that matches
 matching_posts = []
 for i in range(len(ls)):
     post = ls[i]["content"].lower()
@@ -30,12 +32,13 @@ for i in range(len(ls)):
     if find in post:
         matching_posts.append(i)
 
-# print(matching_posts)
-
+# print out of found posts
 print("\n\nMATCHING POSTS: \n")
 for i in matching_posts:
     print(ls[i]["content"])
+
     if ls[i]["attached_image"]:
         print("***THERE IS AN ATTACHED IMAGE***")
+
     print(f"https://www.davidsocial.com/~/thread/{ls[i]["id"]}")
     print()
